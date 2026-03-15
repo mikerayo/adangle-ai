b120f8cb63468cf9acd66f51d8c724bfd22e62da
AdAngle Deploy <deploy@adangle.ai>
2026-03-15 22:21:00 +0000

    Fix CORS for Shopify iframe

--- server.js
+++ server.js
@@ -16,11 +16,17 @@ app.use((req, res, next) => {
   next();
 });
 
-// Middleware
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+// CORS - Allow all origins for Shopify iframe
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
